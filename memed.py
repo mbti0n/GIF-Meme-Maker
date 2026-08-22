5249a58ed32ac2a5d73cbae48521c47e7e0cf62c
mbti0n / Jimmy <alcoholguy.market@gmail.com>
2026-08-22 00:17:43 -0400

    Update memed.py

--- memed.py
+++ memed.py
@@ -6,9 +6,9 @@ from datetime import datetime, timezone
 arguments = argparse.ArgumentParser()
 arguments.add_argument("--path", "-P", type=str, required=True, help="Path to the image.")
 arguments.add_argument("--caption", "-C", type=str, help="GIF text caption. Default is \"caption\" if not specified.")
-arguments.add_argument("--output", "-O", type=str, help="Directory of the output GIF. You will be prompted to specify the output directory if not specified.")
 arguments.add_argument("--width", "-W", type=int, help="GIF width. Default is 720 if not specified.")
 arguments.add_argument("--height", "-H", type=int, help="GIF height. Default is 600 if not specified.")
+arguments.add_argument("--output", "-O", type=str, help="Directory of the output GIF. You will be prompted to specify the output directory if not specified.")
 
 args = arguments.parse_args()
 path = args.path
@@ -45,7 +45,7 @@ def create_image(width, height, caption, path, output):
     try:
         current = datetime.now(timezone.utc)
         timestamp = current.astimezone().strftime("%Y-%m-%d-%H-%M-%S")
-        print("Processing caption...")
+        print("Generating GIF file...")
         im = Image.new(mode="RGB", size=(width, height), color=(255, 255, 255))
         im2 = Image.new(mode="RGB", size=(width, height), color=(0, 0, 0))
         image2 = Image.open(path)
@@ -55,10 +55,8 @@ def create_image(width, height, caption, path, output):
         
         heightText = round(height / 20)
         text.text((width / 2, heightText), caption, fill=(0, 0, 0), font=textFont, anchor="ma", embedded_color=True)
-        print("Processing image...")
         image2 = ImageOps.fit(image2, (width, height-heightText), centering=(0.3, 0.5))
         im.paste(image2, (0, 150))
-        print("Generating GIF file...")
         frames = []
         for i in range(51):
             out = Image.blend(im, im2, (1 - (0.02 * i)))
@@ -66,7 +64,7 @@ def create_image(width, height, caption, path, output):
         for i in range(40):
             frames.append(Image.blend(im, im2, 0))
         frames[0].save(f"{output}/memed-{timestamp}.gif", save_all=True, append_images=frames[1:], duration=40, loop=0)
-        print("Saved!")
+        print(f"Saved! GIF file saved to {output}/memed-{timestamp}.gif")
     except Exception as e:
         print(e)
 
